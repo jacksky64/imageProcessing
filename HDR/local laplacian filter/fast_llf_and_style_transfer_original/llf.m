01d8074c066bfaf95f877c0f7d3f99fb651857f0
jack <giacomocarsana@digiteclecco.com>
2016-10-17 20:41:20 +0200

    HDR local laplacian original

--- HDR/local laplacian filter/fast_llf_and_style_transfer_original/llf.m
+++ HDR/local laplacian filter/fast_llf_and_style_transfer_original/llf.m
@@ -17,39 +17,33 @@
 
 % OUTPUT
 % F : filtered image
-% LP : fitered laplacian pyramid
 
 % aubry.mathieu@gmail.com Sept 2012
 
 
-function [F, LP]=llf(I,sigma,fact,N)
-    if numel(fact) < N
-        fact(end:N)=fact(end);
-    end
-    
-    [height, width]=size(I);
+function [F]=llf(I,sigma,fact,N)
+
+    [height width]=size(I);
     n_levels=ceil(log(min(height,width))-log(2))+2;
     discretisation=linspace(0,1,N);
     discretisation_step=discretisation(2);
     
     input_gaussian_pyr=gaussian_pyramid(I,n_levels);
-    LP=laplacian_pyramid(I,n_levels);
-    LP{n_levels}=input_gaussian_pyr{n_levels};
-    pFact=1;
+    output_laplace_pyr=laplacian_pyramid(I,n_levels);
+    output_laplace_pyr{n_levels}=input_gaussian_pyr{n_levels};
+    
     for ref=discretisation
-        
-        I_remap=fact(pFact)*(I-ref).*exp(-(I-ref).*(I-ref)./(2*sigma*sigma));
-        pFact = pFact+1;
+        I_remap=fact*(I-ref).*exp(-(I-ref).*(I-ref)./(2*sigma*sigma));
         temp_laplace_pyr=laplacian_pyramid(I_remap,n_levels);
         for level=1:n_levels-1
-            LP{level}=LP{level}+...
+            output_laplace_pyr{level}=output_laplace_pyr{level}+...
                 (abs(input_gaussian_pyr{level}-ref)<discretisation_step).*...
                 temp_laplace_pyr{level}.*...
                 (1-abs(input_gaussian_pyr{level}-ref)/discretisation_step);            
         end
     end
     
-    F=reconstruct_laplacian_pyramid(LP);
+    F=reconstruct_laplacian_pyramid(output_laplace_pyr);
     
     
     
--- HDR/local laplacian filter/fast_llf_and_style_transfer_original/llf_general.m
+++ HDR/local laplacian filter/fast_llf_and_style_transfer_original/llf_general.m
@@ -1,13 +1,4 @@
-% Perform the local laplacian filter using the function 
-% f(x,ref)=x+fact*(I-ref)*exp(-(I-ref)²/(2*sigma²))
-
 % Perform the local laplacian filter using any function
-
-% This script implements edge-aware detail and tone manipulation as 
-% described in : 
-% Fast and Robust Pyramid-based Image Processing. 
-% Mathieu Aubry, Sylvain Paris, Samuel W. Hasinoff, Jan Kautz, and Frédo Durand. 
-% MIT technical report, November 2011
 
 % INPUT
 % I : input greyscale image
@@ -17,39 +8,36 @@
 
 % OUTPUT
 % F : filtered image
-% LP : fitered laplacian pyramid
 
 % aubry.mathieu@gmail.com Sept 2012
 
 
-function [F, LP]=llf(I,sigma,fact,N)
-    if numel(fact) < N
-        fact(end:N)=fact(end);
-    end
-    
-    [height, width]=size(I);
+function [F]=llf_general(I,r,N)
+
+    [height width]=size(I);
     n_levels=ceil(log(min(height,width))-log(2))+2;
     discretisation=linspace(0,1,N);
     discretisation_step=discretisation(2);
     
     input_gaussian_pyr=gaussian_pyramid(I,n_levels);
-    LP=laplacian_pyramid(I,n_levels);
-    LP{n_levels}=input_gaussian_pyr{n_levels};
-    pFact=1;
+    output_laplace_pyr=laplacian_pyramid(I,n_levels);
+    output_laplace_pyr{n_levels}=input_gaussian_pyr{n_levels};
+    for level=1:n_levels-1
+            output_laplace_pyr{level}=zeros(size(output_laplace_pyr{level}));
+    end
+    
     for ref=discretisation
-        
-        I_remap=fact(pFact)*(I-ref).*exp(-(I-ref).*(I-ref)./(2*sigma*sigma));
-        pFact = pFact+1;
+        I_remap=r(I-ref);
         temp_laplace_pyr=laplacian_pyramid(I_remap,n_levels);
         for level=1:n_levels-1
-            LP{level}=LP{level}+...
+            output_laplace_pyr{level}=output_laplace_pyr{level}+...
                 (abs(input_gaussian_pyr{level}-ref)<discretisation_step).*...
                 temp_laplace_pyr{level}.*...
                 (1-abs(input_gaussian_pyr{level}-ref)/discretisation_step);            
         end
     end
     
-    F=reconstruct_laplacian_pyramid(LP);
+    F=reconstruct_laplacian_pyramid(output_laplace_pyr);
     
     
     
